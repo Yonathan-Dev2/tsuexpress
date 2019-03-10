5ff263c42dcbb11dc02c55d93a3ea1912fed312c
Yonathan Galindo Galindo <45131635+yonathanbre@users.noreply.github.com>
2019-03-10 12:52:57 -0500

    Se creo cuadro_soporte y fragmento para soporte

---
+++ app/src/main/java/com/cuyesgyg/appcuy/Fragmentos/Fragmento_soporte.java
@@ -0,0 +1,30 @@
+package com.cuyesgyg.appcuy.Fragmentos;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cuyesgyg.appcuy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragmento_soporte extends Fragment {
+
+
+    public Fragmento_soporte() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragmento_soporte, container, false);
+    }
+
+}
