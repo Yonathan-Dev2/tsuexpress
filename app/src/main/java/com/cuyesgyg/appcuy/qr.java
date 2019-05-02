32f0ff4c9f986f0f60a706b26c78f12d047b1698
Yonathan Galindo Galindo <45131635+yonathanbre@users.noreply.github.com>
2019-05-01 22:16:49 -0500

    QR cuyes GYG

--- app/src/main/java/com/cuyesgyg/appcuy/qr.java
+++ app/src/main/java/com/cuyesgyg/appcuy/qr.java
@@ -8,11 +8,13 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.webkit.URLUtil;
+import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -23,12 +25,12 @@ import java.io.IOException;
 
 public class qr extends AppCompatActivity {
 
-
     private CameraSource cameraSource;
     private SurfaceView cameraView;
     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     private String token = "";
     private String tokenanterior = "";
+    TextView mensajeQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,8 @@ public class qr extends AppCompatActivity {
         setContentView(R.layout.activity_qr);
 
         cameraView = (SurfaceView) findViewById(R.id.camera_view);
+        mensajeQR = (TextView)findViewById(R.id.txtmensajeandroid);
+
         initQR();
     }
 
@@ -47,29 +51,27 @@ public class qr extends AppCompatActivity {
                         .setBarcodeFormats(Barcode.ALL_FORMATS)
                         .build();
 
-        // creo la camara
+        // Creo la camara
         cameraSource = new CameraSource
                 .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1600, 1024)
+                .setRequestedPreviewSize(1600, 1080)
                 .setAutoFocusEnabled(true) //you should add this feature
                 .build();
 
-        // listener de ciclo de vida de la camara
+        // Listener de ciclo de vida de la camara
         cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-
                 // verifico si el usuario dio los permisos para la camara
                 if (ActivityCompat.checkSelfPermission(qr.this, Manifest.permission.CAMERA)
                         != PackageManager.PERMISSION_GRANTED) {
-
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         // verificamos la version de ANdroid que sea al menos la M para mostrar
                         // el dialog de la solicitud de la camara
                         if (shouldShowRequestPermissionRationale(
-                                Manifest.permission.CAMERA)) ;
-                        requestPermissions(new String[]{Manifest.permission.CAMERA},
-                                MY_PERMISSIONS_REQUEST_CAMERA);
+                            Manifest.permission.CAMERA));
+                            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
                     }
                     return;
                 } else {
@@ -97,34 +99,35 @@ public class qr extends AppCompatActivity {
             public void release() {
             }
 
-
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
 
                 if (barcodes.size() > 0) {
-
                     // obtenemos el token
                     token = barcodes.valueAt(0).displayValue.toString();
-
                     // verificamos que el token anterior no se igual al actual
                     // esto es util para evitar multiples llamadas empleando el mismo token
                     if (!token.equals(tokenanterior)) {
-
                         // guardamos el ultimo token proceado
                         tokenanterior = token;
                         Log.i("token", token);
 
                         if (URLUtil.isValidUrl(token)) {
-                            // si es una URL valida abre el navegador
+                            // Si es una URL valida abre el navegador
                             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(token));
                             startActivity(browserIntent);
                         } else {
-                            // comparte en otras apps
+                            // Comparte en otras apps
                             Intent shareIntent = new Intent();
                             shareIntent.setAction(Intent.ACTION_SEND);
                             shareIntent.putExtra(Intent.EXTRA_TEXT, token);
                             shareIntent.setType("text/plain");
+                            shareIntent.getDataString();
+
+                            String result = shareIntent.getStringExtra(Intent.EXTRA_TEXT);
+                            mensajeQR.setText(result);
+
                             startActivity(shareIntent);
                         }
 
@@ -143,13 +146,14 @@ public class qr extends AppCompatActivity {
                                 }
                             }
                         }).start();
-
                     }
                 }
             }
         });
 
     }
+
+
 
 
 
